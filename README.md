442a49aff580900ed5ae79d6f51bce1eb91c8a53
ruxxzebre <paullaboratory@gmail.com>
2021-01-13 22:52:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # didactic-spork
-JSON formatter for production purposes.
+
+Didactic Spork
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Testing
+
+To run unit tests:
+
+```sh
+npm test
+```
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
+
---
+++ src/index.js
@@ -0,0 +1,83 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const rotate = document.getElementById('arrowBtn');
+
+rotate.addEventListener('click', () => {
+  const wrapper = document.getElementsByClassName('wrapper')[0];
+  if (wrapper.className === 'wrapper vertical') {
+    wrapper.className = 'wrapper horizontal';
+  } else {
+    wrapper.className = 'wrapper vertical';
+  }
+});
+
+input.focus();
+input.addEventListener('input', (e) => {
+  let obj;
+  try {
+    obj = JSON.parse(e.target.value);
+    obj = processJSON(obj);
+    output.value = JSON.stringify(obj, null, 4);
+    output.select();
+  } catch (e) {
+    output.value = 'It is not valid JSON.';
+  }
+});
+
+const MATCH_TYPES = {
+  array: (object) => Array.isArray(object),
+  boolean: (object) => object.constructor.name === 'Boolean',
+  string: (object) => object.constructor.name === 'String',
+  number: (object) => !isNaN(+object),
+  object: (object) => object.constructor.name === 'Object',
+  null: (object) => object === null,
+}
+
+const MATCH_STRING = {
+  color: (string) => !!string.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/g),
+  date: (string) => !isNaN(Date.parse(string)),
+  email: (string) => !!string.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/gi)
+}
+
+const parseText = (text) => {
+  for (let check in MATCH_STRING) {
+    if (MATCH_STRING[check](text)) {
+      return check.toString();
+    }
+  }
+  return 'text';
+}
+
+const parseType = (value) => {
+  for (let check in MATCH_TYPES) {
+    if (MATCH_TYPES[check](value)) {
+      const stringType = check.toString();
+      if (stringType !== 'string') {
+        return check.toString();
+      } else {
+        return parseText(value);
+      }
+    }
+  }
+  throw new Error('Invalid property type.');
+}
+
+const parseLabel = (name) => {
+  const regex = /([A-Z]*[a-zA-Z]+(?=[_\-\s]?)[a-z]*)/g;
+  const words = name.match(regex);
+  return words.map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join(' ');
+}
+
+const itemConstructor = (key, type) => {
+  return {
+    name: key,
+    type,
+    label: parseLabel(key)
+  }
+}
+
+const processJSON = (jsonObj) => {
+  const result = [];
+  Object.keys(jsonObj).map(item => result.push(itemConstructor(item, parseType(jsonObj[item]))));
+  return result;
+}
