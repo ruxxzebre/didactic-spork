738fb3986bef7fabc77b4a629c7c571e63fe4922
ruxxzebre <paullaboratory@gmail.com>
2021-01-16 20:54:52 +0200

    Reorganization and cleanup

--- src/__tests__/no_label.test.js
+++ src/__tests__/no_label.test.js
@@ -1,5 +1,5 @@
 const testData = require('./test_data_no_label.json');
-const { processJSON } = require('./src/index.js');
+const { processJSON } = require('../index.js');
 
 const { input, output } = testData;
 
