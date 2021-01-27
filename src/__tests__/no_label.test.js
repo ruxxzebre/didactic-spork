0e7e94e93ea78474b9674dc9e3e230c4f196c46d
ruxxzebre <paullaboratory@gmail.com>
2021-01-27 10:23:57 +0200

    clean

--- src/__tests__/no_label.test.js
+++
@@ -1,8 +0,0 @@
-const testData = require('./test_data_no_label.json');
-const { processJSON } = require('../index.js');
-
-const { input, output } = testData;
-
-test('Overcomplicated test', () => {
-  expect(processJSON(input, false)).toEqual(output);
-});
